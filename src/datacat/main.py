33e809bdf38d873e35f71af1bf10e174fe8d514f
Alvaro Clemente <clemente@bigml.com>
2023-08-11 14:02:36 +0200

    feat: basic argument parsing

--- src/datacat/main.py
+++ src/datacat/main.py
@@ -1,21 +1,36 @@
 """Entrypoints for datacat"""
 
+import argparse
 import csv
 import json
+import sys
+from pathlib import Path
 
 Data = list[dict]
 
 
 def main() -> int:
-    path = "data/iris.csv"
+    parser = argparse.ArgumentParser(description="Production like data generator")
+    parser.add_argument("path", type=Path)
+    parser.add_argument("-n", type=int, default=None)
+
+    args = parser.parse_args()
+    path = args.path
+    n = args.n
+
+    # Validate the path
+    if not path.exists():
+        print("data path not found", file=sys.stderr)
+        return 1
+
     data = load_csv(path)
-    show_data(data)
+    show_data(data, n)
     return 0
 
 
-def load_csv(path: str) -> Data:
+def load_csv(path: Path) -> Data:
     """Load the data from a CSV file"""
-    with open(path, "r", newline="") as csvfile:
+    with path.open("r", newline="") as csvfile:
         reader = csv.DictReader(csvfile)
         return list(reader)
 
